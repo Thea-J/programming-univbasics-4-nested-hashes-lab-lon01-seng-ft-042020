3c846a90f9007876de6c9e05f25c9b7230e55640
Dorothy John <dorothyy.john@gmail.com>
2020-04-13 18:02:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,7 +129,8 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-#
+#Add "Assembly" to Dennis Ritchie's languages array 
+#Return the updated hash
 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 programmer_hash
 end
